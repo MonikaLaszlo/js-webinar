8c163ea448febe8d567fd46285e4900873a0d388
Monika Laszlo <monika_laszlo@epam.com>
2020-11-11 11:13:05 +0100

    Add series tasks solution

--- plus/series/lucasNumber.js
+++ plus/series/lucasNumber.js
@@ -6,3 +6,23 @@
  * @param {number} n (n >= 0)
  * @returns {number}
  */
+
+ const lucasNumber = n => {
+    if (typeof n !== 'number'){
+        throw new TypeError('n has to be a number');
+    }
+    if (n < 0){
+        throw new Error('n has to be positive');
+    }
+
+    let lucasN;
+    const lucas = [2, 1];
+
+    for (let i = 2; i <= n; i++){
+        lucas[i] = lucas[i-1] + lucas[i-2];
+    }
+    lucasN = lucas[n];
+    return lucasN;
+ }
+
+ module.exports = lucasNumber;
--- plus/series/tribonacciSequence.js
+++ plus/series/tribonacciSequence.js
@@ -6,3 +6,21 @@
  * @param {number} n (n >= 0)
  * @returns {number}
  */
+
+const tribonacci = n => {
+    if (typeof n !== 'number'){
+        throw new TypeError('n has to be a number');
+    }
+    if (n < 0){
+        throw new Error('n has to be positive');
+    }
+    let tribNumber;
+    const trib = [0, 0, 1];
+
+    for (let i = 3; i <= n; i++){
+        trib[i] = trib[i-1] + trib[i-2] + trib[i-3];
+    }
+    tribNumber = trib[n];
+    return tribNumber;
+}
+module.exports = tribonacci;
