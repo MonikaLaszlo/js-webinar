58ad51be14d3d86bb7762f0180311e215799ef55
Monika Laszlo <monika_laszlo@epam.com>
2020-11-11 11:13:40 +0100

    Add pellNumber solution

--- plus/series/pellNumber.js
+++ plus/series/pellNumber.js
@@ -6,3 +6,23 @@
  * @param {number} n (n >= 0)
  * @returns {number}
  */
+
+const pellNumber = n => {
+    if (typeof n !== 'number'){
+        throw new TypeError('n has to be a number');
+    }
+    if (n < 0){
+        throw new Error('n has to be positive');
+    }
+
+    let pellNum;
+    const pell = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        pell[i] = 2 * pell[i - 1] + pell[i - 2];
+    }
+    pellNum = pell[n];
+    // ...AND THIS COMMENT LINE!
+    return pellNum;
+}
+module.exports = pellNumber;
